66a9a65c70fdca71e339872c6bd7a70afd6c6e65
Muccachino <lucas_staszewski@yahoo.de>
2023-06-04 20:38:46 +0200

    changed styling of GUI a little and added a feature that the letters go in a loop (e.g. a -1 is becoming z)

--- main.js
+++ main.js
@@ -11,13 +11,26 @@ const cipher = () => {
   let textList = [];
   for (let i = 0; i < textInput.length; i++) {
     let afterShift = textInput.charCodeAt(i);
+    console.log(afterShift);
     if (afterShift != 32) {
-      afterShift += shiftNum;
+      if (afterShift > 64 && afterShift < 91) {
+        afterShift = (afterShift - 65 + shiftNum) % 26;
+        console.log(afterShift);
+        if (afterShift < 0) {
+          afterShift += 26;
+        }
+        afterShift += 65;
+      } else if (afterShift > 96 && afterShift < 123) {
+        afterShift = (afterShift - 97 + shiftNum) % 26;
+        if (afterShift < 0) {
+          afterShift += 26;
+        }
+        afterShift += 97;
+      }
     }
     textList.push(String.fromCharCode(afterShift));
   }
   output.value = textList.join("");
-  console.log(output);
   return output;
 };
 
